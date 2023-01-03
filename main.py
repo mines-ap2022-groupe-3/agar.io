987f75b26c326c4cb8d075bc0b5cdbf77ac5e73f
Lidia Ejarque <lidia.ejarque_bueno@etu.minesparis.psl.eu>
2023-01-03 20:26:50 +0100

    correc erreur 3

--- main.py
+++ main.py
@@ -10,28 +10,6 @@ from pygame.color import THECOLORS as COLORS
 import numpy.random as random
 from math import floor
 
-OVERMAP_BG = COLORS["white"]
-BOARD_COLOR = COLORS["grey"]
-BACKGROUND_COLOR = COLORS["black"]
-
-SCREEN = V2(1200, 800)
-WIDTH, HEIGHT = SCREEN
-SCREEN_CENTER = SCREEN / 2
-
-TILE_SIZE = 50
-
-MAP = 2 * SCREEN
-M_WIDTH, M_HEIGHT = MAP
-
-MAX_SPEED = 100
-
-PROBA_APPARITION_FRUIT = 0.05
-NB_MAX_FRUIT = 40
-RAYON_FRUIT_MIN = 5
-RAYON_FRUIT_MAX = 12
-
-BLOB_SIZE_IN = 20
-
 
 def main():
     clock = pg.time.Clock()
