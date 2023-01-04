a1f27f561216eed5f288a300d88acd8569c3e678
Hadrien DO CAO <hadrien.do_cao@edu.minesparis.psl.eu>
2023-01-04 16:15:02 +0100

    ajout tp argparse

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
+import random as rd
 import utilities
 import screen as sc
 import fruit
 import pygame as pg
 from pygame.math import Vector2 as V2
+import argparse
 
 
 def main():
@@ -23,15 +24,23 @@ def main():
     position = V2(sc.MAP) / 2
     SCREEN_CENTER = sc.SCREEN / 2
     MAX_SPEED = 100
-
+    compteur = 0
     # La boucle du jeu
     done = False
     while not done:
         # FPS
         clock.tick(60)
-
+        compteur += 1
         # On trouve la nouvelle direction/position
-        new_direction = V2(pg.mouse.get_pos()) - V2(SCREEN_CENTER)
+        if args.bot:
+            new_direction = V2(pg.mouse.get_pos()) - V2(SCREEN_CENTER)
+        else:
+            if compteur % 20 == 1:
+                x = rd.randint(1, 100)
+                y = rd.randint(1, 100)
+                sgnx = (-1) ** rd.randint(1, 2)
+                sgny = (-1) ** rd.randint(1, 2)
+            new_direction = V2(sgnx * x, sgny * y)
         if new_direction.magnitude() >= MAX_SPEED:
             new_direction = new_direction.normalize()
         else:
@@ -56,6 +65,8 @@ def main():
 
         pg.display.update()
 
+        if args.verbose and compteur % 120 == 0:
+            print(blob_size, position)
         # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
         # ici donc tous les évènements survenus durant la seconde précédente
         for event in pg.event.get():
@@ -74,5 +85,23 @@ def main():
 
 # if python says run, then we should run
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="met le fichier en mode bavard",
+        default=False,
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "-b",
+        "--bot",
+        help="affiche la partie d'un bot",
+        default=True,
+        action="store_false",
+    )
+    args = parser.parse_args()
     main()
     # print(round_to(105, 19))
